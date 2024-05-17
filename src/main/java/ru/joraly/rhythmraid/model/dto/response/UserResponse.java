fe8d7caf0c734b19103b7f30b9f437eca23099a4
Artyom Findin <tyomajoraly@gmail.com>
2024-05-17 09:08:33 +0300

    Add User Request/Response for UserController

---
+++ src/main/java/ru/joraly/rhythmraid/model/dto/response/UserResponse.java
@@ -0,0 +1,10 @@
+package ru.joraly.rhythmraid.model.dto.response;
+
+import lombok.Data;
+
+@Data
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String login;
+}
--- src/main/java/ru/joraly/rhythmraid/rest/UserController.java
+++ src/main/java/ru/joraly/rhythmraid/rest/UserController.java
@@ -4,46 +4,62 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.joraly.rhythmraid.mapper.UserMapper;
 import ru.joraly.rhythmraid.model.User;
+import ru.joraly.rhythmraid.model.dto.request.UserRequest;
+import ru.joraly.rhythmraid.model.dto.response.UserResponse;
 import ru.joraly.rhythmraid.service.impl.UserServiceImpl;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserServiceImpl userService;
+    private final UserMapper userMapper;
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
         List<User> users = userService.getAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
+        List<UserResponse> userResponses = users.stream()
+                .map(userMapper::objectToResponse)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(userResponses, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
         Optional<User> userOptional = userService.getUserById(id);
         if (userOptional.isPresent()) {
-            return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
+            UserResponse userResponse = userMapper.objectToResponse(userOptional.get());
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest) {
+        User user = userMapper.requestToObject(userRequest);
         User createdUser = userService.createUser(user);
-        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        UserResponse userResponse = userMapper.objectToResponse(createdUser);
+        return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody UserRequest userRequest) {
         Optional<User> userOptional = userService.getUserById(id);
         if (userOptional.isPresent()) {
-            user.setId(id);
+            User user = userOptional.get();
+            user.setName(userRequest.getName());
+            user.setLogin(userRequest.getLogin());
+            user.setPassword(userRequest.getPassword());
             User updatedUser = userService.updateUser(user);
-            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+            UserResponse userResponse = userMapper.objectToResponse(updatedUser);
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
